c679ece73f62791df00ade9f3352a848e0fe74cd
NaveenChindam <naveenchindam7777@gmail.com>
2017-06-07 15:30:21 +0530

    Keys

---
+++ Keys.java
@@ -0,0 +1,74 @@
+package keys;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class Keys {
+
+    private KeyPairGenerator keyGen;
+    private KeyPair pair;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public Keys(int keylength)
+        throws NoSuchAlgorithmException, NoSuchProviderException {
+
+        this.keyGen = KeyPairGenerator.getInstance("RSA");
+        this.keyGen.initialize(keylength);
+
+    }
+
+    public void createKeys() {
+
+        this.pair = this.keyGen.generateKeyPair();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+
+    }
+
+    public PrivateKey getPrivateKey() {
+        return this.privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return this.publicKey;
+    }
+
+    public void writeToFile(String path, byte[] key) throws IOException {
+
+        File f = new File(path);
+        f.getParentFile().mkdirs();
+
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(key);
+        fos.flush();
+        fos.close();
+
+    }
+
+    public static void main(String[] args)
+                throws NoSuchAlgorithmException, NoSuchProviderException, IOException {
+
+    	Keys gk_Alice;
+    	Keys gk_Bob;
+
+        gk_Alice = new Keys(1024);
+        gk_Alice.createKeys();
+        gk_Alice.writeToFile("KeyPair4/publicKey_Alice", gk_Alice.getPublicKey().getEncoded());
+        gk_Alice.writeToFile("KeyPair4/privateKey_Alice", gk_Alice.getPrivateKey().getEncoded());
+
+        gk_Bob = new Keys(1024);
+        gk_Bob.createKeys();
+        gk_Bob.writeToFile("KeyPair4/publicKey_Bob", gk_Bob.getPublicKey().getEncoded());
+        gk_Bob.writeToFile("KeyPair4/privateKey_Bob", gk_Bob.getPrivateKey().getEncoded());
+
+    }
+
+}
